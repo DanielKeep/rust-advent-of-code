45fe415bc2e54e5b7f02457f51814592942e2016
Daniel Keep <daniel.keep@gmail.com>
2015-12-14 01:31:36 +1100

    Day 2, part 2

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -2,7 +2,7 @@ use std::io::{BufRead, stdin};
 
 fn main() {
     let stdin = stdin();
-    let area = stdin.lock().lines()
+    let (area, ribbon) = stdin.lock().lines()
         .filter_map(|line| line.ok())
         .filter(|line| line.trim() != "")
         .filter_map(|line| -> Option<(u32, u32, u32)> {
@@ -18,8 +18,12 @@ fn main() {
         .map(|(w, h, l)| {
             use std::cmp::min;
             let (a, b, c) = (w*h, h*l, l*w);
-            2 * (a + b + c) + min(a, min(b, c))
+            let area = 2 * (a + b + c) + min(a, min(b, c));
+            let perim = 2 * min(w+h, min(h+l, l+w));
+            let bow = w*h*l;
+            (area, perim+bow)
         })
-        .fold(0, |a, b| a + b);
+        .fold((0, 0), |a, b| (a.0+b.0, a.1+b.1));
     println!("sq feet of paper: {}", area);
+    println!("feet of ribbon:   {}", ribbon);
 }
