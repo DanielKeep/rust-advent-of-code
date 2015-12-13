cbb6d930f874b73b47ed2fc4028e22a476adc08d
Daniel Keep <daniel.keep@gmail.com>
2015-12-14 00:46:51 +1100

    Day 1, part 2

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,13 +1,18 @@
 use std::io::{Read, stdin};
 
 fn main() {
-    let floor = stdin().bytes()
+    let (floor, basement) = stdin().bytes()
         .filter_map(|b| match b {
             Ok(b'(') => Some(1),
             Ok(b')') => Some(-1),
             _ => None
         })
-        .fold(0, |a, b| a + b);
+        .enumerate()
+        .fold((0, None), |(a, p), (i, b)| {
+            let f = a + b;
+            (f, if f == -1 { p.or(Some(i+1)) } else { p })
+        });
 
     println!("floor: {}", floor);
+    println!("position: {:?}", basement);
 }
