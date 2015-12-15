b5e470e11e29735a9a24135e3a9afac45595e41f
Daniel Keep <daniel.keep@gmail.com>
2015-12-15 18:25:22 +1100

    Day 5, part 2

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -1,5 +1,7 @@
 #[macro_use] extern crate lazy_static;
+extern crate clap;
 extern crate regex;
+extern crate strcursor;
 
 use std::io::{BufRead, stdin};
 use regex::Regex;
@@ -16,31 +18,77 @@ lazy_static! {
 }
 
 fn main() {
+    let new_rules = args();
+
     let stdin = stdin();
     let nice_strings = stdin.lock().lines()
         .filter_map(Result::ok)
-        .map(|line| is_nice(&line))
+        .map(|line| is_nice(&line, new_rules))
         .fold(0, |count, is_nice| if is_nice { count + 1 } else { count });
 
     println!("nice strings: {}", nice_strings);
 }
 
-fn is_nice(s: &str) -> bool {
+fn args() -> bool {
+    let matches = clap::App::new("day5")
+        .args_from_usage("\
+            -n --new-rules 'Use new rules'\
+        ")
+        .get_matches();
+
+    let new_rules = matches.is_present("new-rules");
+
+    new_rules
+}
+
+fn is_nice(s: &str, new_rules: bool) -> bool {
     let s = s.trim();
 
-    if !RE_VOWELS.is_match(s) {
-        return false;
-    }
+    if new_rules {
+        if !(|| {
+            use strcursor::StrCursor;
 
-    if RE_NAUGHTY.is_match(s) {
-        return false;
-    }
+            let mut s = &s[..];
+            while s.len() >= 4 {
+                let mut cur = StrCursor::new_at_start(s);
+                cur.seek_next();
+                let s_next = cur.slice_after();
+                cur.seek_next();
+                let head = cur.slice_before();
+                if cur.slice_after().contains(head) {
+                    return true;
+                } else {
+                    s = s_next;
+                }
+            }
 
-    for (c0, c1) in s.chars().zip(s.chars().skip(1)) {
-        if c0 == c1 {
-            return true;
+            false
+        })() {
+            return false;
         }
-    }
 
-    false
+        for (c0, c2) in s.chars().zip(s.chars().skip(2)) {
+            if c0 == c2 {
+                return true;
+            }
+        }
+
+        false
+    } else {
+        if !RE_VOWELS.is_match(s) {
+            return false;
+        }
+
+        if RE_NAUGHTY.is_match(s) {
+            return false;
+        }
+
+        for (c0, c1) in s.chars().zip(s.chars().skip(1)) {
+            if c0 == c1 {
+                return true;
+            }
+        }
+
+        false
+    }
 }
