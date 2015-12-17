f829b3b0bb1e15eb5f8b6e3c2906c66406870233
Daniel Keep <daniel.keep@gmail.com>
2015-12-18 02:54:40 +1100

    Day 12, part 2

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -1,18 +1,33 @@
+extern crate clap;
 extern crate serde_json;
 
 use serde_json::Value;
 
 fn main() {
+    let ignore_red = args();
+
     let input = read_stdin();
     let input: Value = serde_json::from_str(&input).unwrap();
 
     let mut sum = 0.0;
-    for_each_number(&input, &mut |v| sum += v);
+    for_each_number(&input, ignore_red, &mut |v| sum += v);
 
     println!("sum: {}", sum);
 }
 
-fn for_each_number<F>(value: &Value, f: &mut F)
+fn args() -> bool {
+    let matches = clap::App::new("day12")
+        .args_from_usage("\
+            -i --ignore-red 'Ignore objects with `red` property'\
+        ")
+        .get_matches();
+
+    let ignore_red = matches.is_present("ignore-red");
+
+    ignore_red
+}
+
+fn for_each_number<F>(value: &Value, ignore_red: bool, f: &mut F)
 where F: FnMut(f64) {
     match *value {
         Value::F64(v) => f(v),
@@ -21,13 +36,21 @@ where F: FnMut(f64) {
 
         Value::Array(ref values) => {
             for value in values {
-                for_each_number(value, f);
+                for_each_number(value, ignore_red, f);
             }
         },
 
         Value::Object(ref fields) => {
-            for value in fields.values() {
-                for_each_number(value, f);
+            if {
+                !fields.values()
+                    .any(|v| match *v {
+                        Value::String(ref s) => &**s == "red",
+                        _ => false
+                    })
+            } {
+                for value in fields.values() {
+                    for_each_number(value, ignore_red, f);
+                }
             }
         },
 
