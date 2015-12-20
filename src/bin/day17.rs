478253c63336ba14b3706fce0b787414283a66f0
Daniel Keep <daniel.keep@gmail.com>
2015-12-21 00:16:36 +1100

    Day 17, part 2

--- src/bin/day17.rs
+++ src/bin/day17.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let (show_solutions, liters) = args();
+    let (show_solutions, min_num, liters) = args();
     let containers: Vec<u32> = read_stdin().lines()
         .map(|s| s.parse().unwrap())
         .collect();
@@ -7,9 +7,17 @@ fn main() {
     let mut solutions = vec![];
     search(&containers, liters, &mut solutions);
 
+    if min_num {
+        solutions.sort_by(|a, b| a.len().cmp(&b.len()));
+        if let Some(min_len) = solutions.get(0).map(|e| e.len()) {
+            solutions.retain(|e| e.len() == min_len);
+        }
+    }
+
     println!("{} solutions{}",
         solutions.len(),
         if show_solutions { ":" } else { "" });
+
     if show_solutions {
         for soln in solutions {
             println!("- {:?}", soln);
@@ -49,22 +57,24 @@ fn search(cont: &[u32], liters: u32, solns: &mut Vec<Vec<u32>>) {
     step(vec![], cont, liters, solns);
 }
 
-fn args() -> (bool, u32) {
+fn args() -> (bool, bool, u32) {
     extern crate clap;
 
     let matches = clap::App::new("day17")
         .args_from_usage("\
             -s --solutions 'Show solutions'
+            -m --min-num 'How many ways can be minimum number of containers be filled'
             <LITERS> 'Liters of eggnog'\
         ")
         .get_matches();
 
     let solutions = matches.is_present("solutions");
+    let min_num = matches.is_present("min-num");
     let liters = matches.value_of("LITERS")
         .map(|s| s.parse().unwrap())
         .unwrap();
 
-    (solutions, liters)
+    (solutions, min_num, liters)
 }
 
 fn read_stdin() -> String {
